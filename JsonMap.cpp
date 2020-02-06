801e813a83da2486c86e295e969d35fd668aa6cf
Balgy <kakoutopoulos@gmail.com>
2020-02-06 19:16:11 +0200

    Fixed an issue with the initial commit

--- JsonMap.cpp
+++ JsonMap.cpp
@@ -1,48 +1,133 @@
-#include "CoreMinimal.h"
-#include "Misc/TVariant.h"
-#include <initializer_list>
+#include "JsonMap.h"
 
-class FJsonMapValue
+FJsonMap::FJsonMap()
 {
-public:
-	FJsonMapValue();
+	
+}
 
-	FJsonMapValue(int32 InInt);
-	FJsonMapValue(float InFloat);
-	FJsonMapValue(const FString& InString);
-	FJsonMapValue(bool InBool);
-
-	TVariant<FString, int32, bool, float > Get() const;
-
-	FString ToString();
-private:
-	TVariant<FString, int32, bool, float > InternalValue;
-};
-
-class FJsonMapInput
+FJsonMap::FJsonMap(std::initializer_list<FJsonMapInput> InList)
 {
-public:
-	FJsonMapInput();
+	for (auto& Elem : InList)
+	{
+		InternalObjectMap.Add(Elem.Get());
+	}
+}
 
-	FJsonMapInput(const char* InKeyName, int32 InInt);
-	FJsonMapInput(const char* InKeyName, float InFloat);
-	FJsonMapInput(const char* InKeyName, const char* InString);
-	FJsonMapInput(const char* InKeyName, const FString& InString);
-	FJsonMapInput(const char* InKeyName, bool InBool);
-
-	TTuple<FString, FJsonMapValue > Get() const;
-private:
-	TTuple<FString, FJsonMapValue > InternalValue;
-};
-
-class FJsonMap
+FString FJsonMap::ToString()
 {
-public:
-	FJsonMap();
-	FJsonMap(std::initializer_list<FJsonMapInput> InList);
+	TSharedRef<FJsonObject> JsonObject = MakeShared<FJsonObject>();
 
-	FString ToString();
+	for (auto& Elem : InternalObjectMap)
+	{
+		auto ElemValue = Elem.Value.Get();
+		if (ElemValue.IsType<int32>())
+		{
+			JsonObject->SetNumberField(Elem.Key, ElemValue.Get<int32>());
+		}
+		else if (ElemValue.IsType<FString>())
+		{
+			JsonObject->SetStringField(Elem.Key, ElemValue.Get<FString>());
+		}
+		else if (ElemValue.IsType<bool>())
+		{
+			JsonObject->SetBoolField(Elem.Key, ElemValue.Get<bool>());
+		}
+		else if (ElemValue.IsType<float>())
+		{
+			JsonObject->SetNumberField(Elem.Key, ElemValue.Get<float>());
+		}
+	}
 
-private:
-	TMap<FString, FJsonMapValue> InternalObjectMap;
-};
+	FString OutputString = "";
+	TSharedRef< TJsonWriter<> > Writer = TJsonWriterFactory<>::Create(&OutputString);
+	FJsonSerializer::Serialize(JsonObject, Writer);
+
+	return OutputString;
+}
+
+FJsonMapValue::FJsonMapValue()
+{
+	InternalValue.Set<int32>(0);
+}
+
+FJsonMapValue::FJsonMapValue(int32 InInt)
+{
+	InternalValue.Set<int32>(InInt);
+}
+
+FJsonMapValue::FJsonMapValue(float InFloat)
+{
+	InternalValue.Set<float>(InFloat);
+}
+
+FJsonMapValue::FJsonMapValue(const FString& InString)
+{
+	InternalValue.Set<FString>(InString);
+}
+
+FJsonMapValue::FJsonMapValue(bool InBool)
+{
+	InternalValue.Set<bool>(InBool);
+}
+
+TVariant<FString, int32, bool, float > FJsonMapValue::Get() const
+{
+	return InternalValue;
+}
+
+FString FJsonMapValue::ToString()
+{
+	if (InternalValue.IsType<int32>())
+	{
+		return FString::FromInt(InternalValue.Get<int32>());
+	}
+	else if (InternalValue.IsType<FString>())
+	{
+		return InternalValue.Get<FString>();
+	}
+	else if (InternalValue.IsType<bool>())
+	{
+		return FString::FromInt(InternalValue.Get<bool>());
+	}
+	else if (InternalValue.IsType<float>())
+	{
+		return FString::SanitizeFloat(InternalValue.Get<float>());
+	}
+
+	return "";
+}
+
+FJsonMapInput::FJsonMapInput()
+{
+
+}
+
+FJsonMapInput::FJsonMapInput(const char* InKeyName, int32 InInt)
+{
+	InternalValue = TTuple<FString, FJsonMapValue >(FString(InKeyName), FJsonMapValue(InInt));
+}
+
+FJsonMapInput::FJsonMapInput(const char* InKeyName, float InFloat)
+{
+	InternalValue = TTuple<FString, FJsonMapValue >(FString(InKeyName), FJsonMapValue(InFloat));
+}
+
+FJsonMapInput::FJsonMapInput(const char* InKeyName, const char* InString)
+{
+	InternalValue = TTuple<FString, FJsonMapValue >(FString(InKeyName), FJsonMapValue(FString(InString)));
+}
+
+FJsonMapInput::FJsonMapInput(const char* InKeyName, bool InBool)
+{
+	InternalValue = TTuple<FString, FJsonMapValue >(FString(InKeyName), FJsonMapValue(InBool));
+}
+
+FJsonMapInput::FJsonMapInput(const char* InKeyName, const FString& InString)
+{
+	InternalValue = TTuple<FString, FJsonMapValue >(FString(InKeyName), FJsonMapValue(InString));
+}
+
+TTuple<FString, FJsonMapValue > FJsonMapInput::Get() const
+{
+	return TTuple<FString, FJsonMapValue >(InternalValue.Key, FJsonMapValue(InternalValue.Value));
+}
